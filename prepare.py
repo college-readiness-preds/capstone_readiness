51132caa185cd0f55057a3fe368d9700863a6bec
carlopdeleon <carlo.pagarigan.deleon@gmail.com>
2023-03-14 10:42:31 -0500

    py module

---
+++ prepare.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+
+#--------------------------------------------------------------------------------------------------
+
+def convert_dollars_to_float(x):
+    fix = []
+    blah = []
+    something = [] #this list holds only the balance keys from the dictionary
+    only_dollars = [] # this holds the values without the commas
+    convert = []
+    for d in x:
+        d = d.replace(' ', '')
+        fix.append(d)
+    for e in fix:
+        e = e.replace('-', '')
+        blah.append(e)
+        # this holds the converted values 
+    for a in blah:
+        a = a.strip('$')
+        something.append(a)
+    for b in something:
+        b = b.replace(",","")
+        only_dollars.append(b)
+    for c in only_dollars:
+        c = float(c)
+        convert.append(c)
+
+    return convert
+
+#--------------------------------------------------------------------------------------------------
+
+def change_dollars(df):
+    school = school[school['salary'] != '-']
+    school = school[school['salary'] != '?']
+    school.salary = convert_dollars_to_float(school.salary)
+    school.all_fund = convert_dollars_to_float(school.all_fund)
+    school.extra_fund = convert_dollars_to_float(school.extra_fund)
+    return df
+
+#--------------------------------------------------------------------------------------------------
+
+def remove_percent_sign(df):
+    
+    '''
+    Remove percent sign from all string values in the columns of a pandas DataFrame.
+    '''
+
+    # Columns to loop
+    loop_columns = list(df.columns[2:])
+    
+    # Loop to remover '%' sign
+    for col in loop_columns:
+        try:
+            df[col] = df[col].str.replace('%','')
+            
+        except:
+            continue
+            
+    return df
