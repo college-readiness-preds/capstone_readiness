e426d92cbbb2e5287b83b84849af11881d3e51e4
carlopdeleon <carlo.pagarigan.deleon@gmail.com>
2023-03-15 16:09:19 -0500

    model py

--- modeling.py
+++ modeling.py
@@ -9,13 +9,15 @@ def the_trains(train,validate,test, target='target'):
     '''
     Creates X and y trains.
     '''
-    
+    # X and y trains sets
     X_train = train.drop([target], axis=1)
     y_train = train[target]
 
+    # X and y validates sets
     X_validate = validate.drop([target], axis=1)
     y_validate = validate[target]
 
+    # X and y test sets
     X_test = test.drop([target], axis=1)
     y_test = test[target]
     
